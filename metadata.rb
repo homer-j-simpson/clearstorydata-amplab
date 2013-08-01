b37ba3b735cc54ffde5a12e9305f075f3cd0e6ed
Mikhail Bautin <mbautin@gmail.com>
2013-08-01 11:53:26 -0700

    Update clearstorydata cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email "mbautin@clearstorydata.com"
 license          "All rights reserved"
 description      "ClearStory Data AmpLab stack recipes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.1.0"
 
 recipe "clearstorydata-amplab::tachyon-install", "Installs Tachyon on the node"
 recipe "clearstorydata-amplab::tachyon-master", "Sets up Tachyon master using Monit"
 recipe "clearstorydata-amplab::tachyon-worker", "Sets up Tachyon worker using Monit"
 
-depends "clearstorydata", "~> 0.11.0"
+depends "clearstorydata", "~> 1.0.1"
 depends "clearstorydata-monit", "~> 1.3.2"
 depends "cloudera", "~> 1.2.2"
 
