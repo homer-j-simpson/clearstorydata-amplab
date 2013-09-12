a3ccd869b42d17af7ab78bb42576d7bc3fd45236
Mikhail Bautin <mbautin@gmail.com>
2013-09-12 13:52:35 -0700

    Bump clearstorydata cookbook version and use x.y constraints

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,14 @@ maintainer_email "mbautin@clearstorydata.com"
 license          "All rights reserved"
 description      "ClearStory Data AmpLab stack recipes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.4.1"
+version          "1.4.2"
 
 recipe "clearstorydata-amplab::tachyon-install", "Installs Tachyon on the node"
 recipe "clearstorydata-amplab::tachyon-master", "Sets up Tachyon master using Monit"
 recipe "clearstorydata-amplab::tachyon-worker", "Sets up Tachyon worker using Monit"
 
-depends "clearstorydata", "~> 1.3.0"
-depends "clearstorydata-monit", "~> 1.2.0"
+depends "clearstorydata", "~> 1.4"
+depends "clearstorydata-monit", "~> 1.2"
 depends "cloudera", "~> 1.4"
 
 %w{ debian ubuntu }.each do |os|
