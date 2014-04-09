97c5f9fc5555b71b40c83b6662fd0818516f7680
Mikhail Bautin <mbautin@gmail.com>
2014-04-09 11:52:20 -0700

    Bump major version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "mbautin@clearstorydata.com"
 license          "All rights reserved"
 description      "ClearStory Data AmpLab stack recipes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.5.0"
+version          "2.0.0"
 
 recipe "clearstorydata-amplab::tachyon-install", "Installs Tachyon on the node"
 recipe "clearstorydata-amplab::tachyon-master", "Sets up Tachyon master using Monit"
